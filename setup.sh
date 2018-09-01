176ed77851e4af760ea0cde2f1b7980b0f9fb8db
Michael Pavlukhin <alivemihail@gmail.com>
2018-09-01 17:36:17 +0300

    update

--- setup.sh
+++ setup.sh
@@ -6,4 +6,7 @@ apt-get install python3.6
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6
 update-alternatives --config python3
+apt-get install -y python3-pip
+apt-get install build-essential libssl-dev libffi-dev python-dev
+apt-get install -y python3-venv
 pip3 install -r requirements.txt
